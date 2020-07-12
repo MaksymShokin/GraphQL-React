3ef0d0305ea162313a9c5d22b9cbb0a09b114699
Maksym Shokin <maksym.sh@upplabs.com>
2020-07-12 19:14:33 +0300

    small-fix

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -204,7 +204,18 @@ class Feed extends Component {
           createdAt: resData.data.createPost.createdAt
         };
         this.setState(prevState => {
+          let updatedPosts = [...prevState.posts];
+          if (prevState.editPost) {
+            const postIndex = prevState.posts.findIndex(
+              p => p._id === prevState.editPost._id
+            );
+            updatedPosts[postIndex] = post;
+          } else {
+            updatedPosts.pop();
+            updatedPosts.unshift(post);
+          }
           return {
+            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false
